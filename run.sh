c1ad309bac570aeb10c81776c4c18c0c0127b3e1
Stefano Bragg <70184158+Braggstefano@users.noreply.github.com>
2025-01-21 18:47:08 +0800

    Delete run.sh

--- run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# This is a script to run the sTPB model with custom parameters
-
-# Command to run the Python script with desired parameters
-python3 run.py --G_0 1.0 --C_0 6.0 --gamma 0.1 --N 10
