8edcc48ebd197f70b87f33edb59ac8b2685c0de9
Stefano Bragg <70184158+Braggstefano@users.noreply.github.com>
2025-01-22 12:19:32 +0800

    Update run.py

--- run.py
+++ run.py
@@ -20,46 +20,6 @@ def parse_args():
     return args
 
 
-# def train(metric, train_sessions):
-#     min_TSE = 1e5
-#     best_parameters = {}
-#     parameters_range = {}
-#     if metric == 'sTPB':
-#         parameters_range['G_0'] = np.arange(0.1, 5.01, 0.1)
-#         parameters_range['C_0'] = np.arange(1.0, 10.1, 1)
-#         parameters_range['gamma'] = np.arange(0.0, 1.01, 0.01)
-        
-#     examine_behaviour_dist, query_behaviour_dist, max_session_len = get_examine_query_behavior_dist(train_sessions)
-#     parameters_names = list(parameters_range.keys())
-#     if metric == 'sTPB':
-#         for parameter0, parameter1,parameter2 in itertools.product(parameters_range[parameters_names[0]],
-#                                                     parameters_range[parameters_names[1]],
-#                                                     parameters_range[parameters_names[2]],
-#                                                     ):
-#             parameters = {parameters_names[0]: parameter0, parameters_names[1]: parameter1,  parameters_names[2]: parameter2}
-#             examine_model_dist, query_model_dist = get_examine_and_query_model_dist(metric, parameters, train_sessions, max_session_len)
-#             TSE_examine = np.sum(np.square(examine_behaviour_dist - examine_model_dist))
-#             TSE_query = np.sum(np.square(query_behaviour_dist - query_model_dist))*10
-#             TSE = (TSE_examine + TSE_query)*100
-#             if TSE < min_TSE:
-#                 min_TSE = TSE
-#                 best_parameters = parameters
-                
-#     return best_parameters
-
-
-# def test(metric, parameters, test_sessions):
-#     length_of_test_sessions = len(test_sessions)
-#     examine_behaviour_dist, query_behaviour_dist, max_session_len = get_examine_query_behavior_dist(test_sessions)
-#     examine_model_dist, query_model_dist = get_examine_and_query_model_dist(metric, parameters, test_sessions, max_session_len)
-#     MSE_examination = np.sum(np.square(examine_behaviour_dist - examine_model_dist))/length_of_test_sessions
-#     MSE_reformulation = np.sum(np.square(query_behaviour_dist - query_model_dist))/length_of_test_sessions
-#     spearman_corr, pearson_corr = metric_correlation_coefficient(test_sessions, examine_model_dist)
-#     return MSE_examination, MSE_reformulation, spearman_corr, pearson_corr
-
-
-
-
 def main():
     args = parse_args()
     
